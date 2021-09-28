2aaaa0283388b3bd34a680434cb9026ee940e909
jedreky <jkaniewski@fuw.edu.pl>
2021-09-28 16:57:21 +0200

    Delete examples.py

--- reviews/examples.py
+++
@@ -1,7 +0,0 @@
-"""
-This file contains some reviews which are not part of our dataset.
-"""
-
-review1 = 'This is a truly fantastic movie and I would like to watch it again. Stefan is a great director, noone can compete with him!'
-review2 = 'This is an ok movie, but I have seen better. Moreover, it gets quite boring towards the end.'
-review3 = 'This is a completely terrible movie, I have never seen anything worst in my life.'
